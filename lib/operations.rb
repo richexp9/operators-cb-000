c8b56d24e5c20177f08a9dc3ea1a9c8b283dfaed
Richard Loera <richexp9@gmx.com>
2018-01-13 05:29:59 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,24 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  elsif speed <40
+    return true
+  else
+    return false
+  end
+  # speed < 40 ? return true : return false
 end
 
 
 
 def not_safe?(speed)
-	
+  # if speed > 60
+  #   return true
+  # elsif speed < 40
+  #   return true
+  # else
+  #   return false
+  # end
+  speed > 60 ? true : speed < 40
+  # speed < 40 ? return true : return false
 end
-	
-
-
