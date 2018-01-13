378ce631feb0757a29fdffdf5ab35228b6ed83fc
Richard Loera <richexp9@gmx.com>
2018-01-13 05:27:01 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,11 +12,12 @@ end
 
 
 def not_safe?(speed)
-  if speed > 60
-    return true
-  elsif speed < 40
-    return true
-  else
-    return false
-  end
+  # if speed > 60
+  #   return true
+  # elsif speed < 40
+  #   return true
+  # else
+  #   return false
+  # end
+  speed > 60 ? true : false
 end
